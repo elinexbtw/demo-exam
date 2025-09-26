58c636a09681226763c8666de3cd6520f53c5963
kabinet303 <kabinet303@localhost.localdomain>
2025-09-26 09:35:05 +0300

    Базовая структура сайта

---
+++ moy_ne_sam/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>мой не сам</title>
+    <link rel='icon' href='images/nugget.jpeg'>
+    <link rel='stylesheet' href='style/style.css'>
+</head>
+<body>
+    <img src='images/nugget.jpeg' alt='логотип'>
+    <h1> Мой не сам </h1>
+</headre>
+<nav>
+    <a href='/demo-exam/moy_ne_sam'>Главное</a>
+    <a href='/demo-exam/moy_ne_sam/admin'>Админ</a>
+</nav>
+<main>
+</main>
+<footer>2025</footer>
+    <script src='script/script.js'></script>
+</body>
+</html>
